a9b7be2ef1cec23fd259d18893012f11bf1b3f6d
cmontague4 <90429535+cmontague4@users.noreply.github.com>
2023-04-19 12:14:50 -0400

    Create main.cpp

---
+++ Scaffolding/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main(int argc, char* argv[]) {
+
+
+    // This main will be included in each of the 3 shape files, but the
+    // CLAs and body will vary slightly depending on which shape it's being used for
+
+
+    // std::string out_string = shape_function(shape function inputs)
+    //std::ofstream out_file(argv[x]);
+
+    /*
+    for (int i = 0 ; i < out_string.size() ; i++) {
+        out_file << out_string[i] << " ";
+    }
+    */
+
+
+}
